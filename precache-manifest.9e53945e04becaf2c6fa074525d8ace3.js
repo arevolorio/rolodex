841585185d6111cb3c2f1750d2eda62d34ab0271
Allan Revolorio <alanrevolorio@gmail.com>
2020-01-06 22:42:43 -0600

    Updates

---
+++ precache-manifest.9e53945e04becaf2c6fa074525d8ace3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4369da116626489a80e33132f178e109",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "321d25ab1d6504b4f1c1",
+    "url": "/rolodex/static/css/main.a4c08be7.chunk.css"
+  },
+  {
+    "revision": "ff0178b2c039e839d26a",
+    "url": "/rolodex/static/js/2.0a2f46cf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.0a2f46cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "321d25ab1d6504b4f1c1",
+    "url": "/rolodex/static/js/main.60fe0ef2.chunk.js"
+  },
+  {
+    "revision": "f092306fb1ae0ff8129f",
+    "url": "/rolodex/static/js/runtime-main.aea65a69.js"
+  }
+]);
